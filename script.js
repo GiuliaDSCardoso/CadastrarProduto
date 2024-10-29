d350d79383e7723be27f4ea594d990a1850ad5ee
Giulia Cardoso <imgirassoia@gmail.com>
2024-10-29 15:36:48 -0300

    1.0.1

--- script.js
+++ script.js
@@ -12,8 +12,28 @@ function fetchProducts() {
                 <p>Price: ${product.price}</p>
                 <p>${product.description}</p>
                 <button onclick="deleteProduct(${product.id})">Delete</button>
-                `
+                <button onclick="editProduct(${product.id}), '${product.name}',
+                ${product.price}, '${product.descriptio}')">Edit</button>`;
+                productList.appendChild(div);
         })
         })
+    }
 
-    })
+    function deleteProduct(id){
+        if(confirm('Tem certeza que deseja excluir este produto? ')){
+            fetch(`delete_product.php?id=${id}`, {method: 'GET'})
+            .then( ()=> fetchProducts());
+        }
+    }
+
+
+    function editProduct(id, name, price, description) {
+        document.getElementById('name').value = name;
+        document.getElementById('price').value = price;
+        document.getElementById('description').value = description;
+
+        const form = document.getElementById('productForm');
+        form.action = `edit_product.php?id=${id}`;
+    }
+    window.onload = fetchProducts;  
+    
